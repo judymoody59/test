2deeb9581c78b214c67f0252447ebe1023bed0d0
MinJu <108432112+judymoody59@users.noreply.github.com>
2024-02-01 10:06:26 +0900

    Feat: 프로젝트 파일 추가
    Ewha Recipe 프로젝트 파일 추가함

---
+++ ewharecipe/src/main/java/com/example/ewharecipe/EwharecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ewharecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwharecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwharecipeApplication.class, args);
+	}
+
+}
---
+++ ewharecipe/src/test/java/com/example/ewharecipe/EwharecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ewharecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwharecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
